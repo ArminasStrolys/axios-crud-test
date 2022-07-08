58f9bc920e8040fb0aa7d2cf46a0554b32a85db7
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-07-08 19:23:35 +0300

    Get request in progress

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,19 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import User from "../user/User";
 
 const Main = () => {
+  const [userData, setUserData] = useState([]);
 
-    const [userData, setUserData] = useState([])
- 
-    useEffect(() => {
-        Axios.get("https://random-data-api.com/api/users/random_user?size=5").then((data) => {
-            setUserData(data);
-        });
-      }, []);
-    
+  useEffect(() => {
+    Axios.get("https://random-data-api.com/api/users/random_user?size=5").then(
+      (data) => {
+        setUserData(data);
+      }
+    );
+  }, []);
+
+  console.log(userData.data);
+
   return (
     <div>
       <h2 style={{ textAlign: "center" }}>Main page</h2>
+      <br />
+      {userData.length > 1 &&
+        userData.map((user) => <User name={user.first_name} />)}
+      <User />
     </div>
   );
 };
