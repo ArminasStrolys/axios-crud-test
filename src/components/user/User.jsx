6a5afe29ada972e4d62ecc68a5d7974c30f929a0
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-07-09 23:39:38 +0300

    Update User.jsx

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -11,14 +11,40 @@ const User = (props) => {
         backgroundColor: "lightGray",
       }}
     >
-      <img style={{ width: "70px", float:"left" }} src={props.avatar} alt="" />
+      <img
+        style={{ width: "70px", float: "left" }}
+        src={props.avatar}
+        alt="Something went wrong"
+      />
       <span style={{ position: "relative", right: "70px", bottom: "18px" }}>
-        <b>{props.name} {props.surname}</b>
+        <b>
+          {props.name} {props.surname}
+        </b>
       </span>
-      <p style={{position:"relative", bottom:"30px", left:"20px"}}><b>Address:</b> {props.city}, {props.state}. {props.street}.</p>
-      <p style={{position:"relative", bottom:"40px", left:"20px"}}><b>Gender:</b> {props.gender}.</p>
-      <p style={{position:"relative", bottom:"50px", left:"90px"}}><b>Soc. num.:</b> {props.socialId}.</p>
-      <button style={{position:"relative", bottom:"112px", right:"40px", backgroundColor: "white", width:"20px", height:"20px", textAlign:"center", color: "red", lineHeight:"10px"}}>+</button>
+      <p style={{ position: "relative", bottom: "30px", left: "20px" }}>
+        <b>Address:</b> {props.city}, {props.state}. {props.street}.
+      </p>
+      <p style={{ position: "relative", bottom: "40px", left: "20px" }}>
+        <b>Gender:</b> {props.gender}.
+      </p>
+      <p style={{ position: "relative", bottom: "50px", left: "90px" }}>
+        <b>Soc. num.:</b> {props.socialId}.
+      </p>
+      <button
+        style={{
+          position: "relative",
+          bottom: "112px",
+          right: "40px",
+          backgroundColor: "white",
+          width: "20px",
+          height: "20px",
+          textAlign: "center",
+          color: "red",
+          lineHeight: "10px",
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
