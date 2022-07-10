c5fb3257d6ae7b96ef183122d4d2668b3a9f0fca
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-07-10 18:23:28 +0300

    Write to .txt file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 const fs = require('fs');
 
-let path = '../../src/data/data.txt';
+let path = '../src/data/data.txt';
+let buffer = new Buffer('testing');
+
+fs.open(path, 'w', function(err, fd) {
+    if (err) {
+        throw 'could not open file: ' + err;
+    }
+
+    fs.write(fd, buffer, 0, buffer.length, null, function(err) {
+        if (err) throw 'error writing file: ' + err;
+        fs.close(fd, function() {
+            console.log('wrote successfully');
+        });
+    });
+});
 
 app.use(express.json());
 
